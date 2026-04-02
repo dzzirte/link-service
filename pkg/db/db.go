d6bb9bac58a3200b0dd7f9c42cdf82d9baed825a
dzzirte <illuvator@list.ru>
2026-04-02 19:03:48 +0300

    last fix

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"awesomeproject/configs"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -12,7 +13,11 @@ type Db struct {
 }
 
 func NewDb(conf *configs.Config) *Db {
-	db, err := gorm.Open(postgres.Open(conf.Db.DSN), &gorm.Config{})
+	connStr := os.Getenv("DATABASE_URL")
+	if connStr == "" {
+		connStr = conf.Db.DSN
+	}
+	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
